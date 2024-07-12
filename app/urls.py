ee3cbd7d073acaa07fa5e3ab8926dd1b7e06144b
sanchita <sanchita15pa@gmail.com>
2024-07-12 18:04:12 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
+    path('login/',login,name='login')
+
+]
